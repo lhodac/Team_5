6d363e4c065bb7213b581baa74ad3fb19762e5ea
lhodac <32448517+lhodac@users.noreply.github.com>
2017-11-14 13:58:27 +0100

    Add files via upload
    Ajout d'un google test sur la fonction readFromFile
    Ajout du fichier "test.fa" sur lequel est basé le google test (fourni par les enseignants)

--- test/test/googletests.cpp
+++ test/test/googletests.cpp
@@ -4,6 +4,25 @@
 #include "Allele.hpp"
 #include "gtest/gtest.h"
 
+/*! Test if the table that readFromFile function returns contains the nucleotides choser by the marker site table
+ * (test on the "test.fa" file)
+ */
+TEST (SimulationTest, readFromFile_) 
+{
+	Simulation sim;
+	ifstream inputFile;
+	inputFile.open("test.fa");
+	
+	vector<int> test = {0,2,4,6};
+	vector<string> Alleles;
+	Alleles = readFromFile(test, inputFile);
+	
+	EXPECT_EQ(Alleles[0], "TCAC");
+	EXPECT_EQ(Alleles[1], "TCAC");
+	EXPECT_EQ(Alleles[2], "TCTC");
+
+	
+}
 
 int main(int argc, char **argv) 
 {
